944f9c4bc3d54105c331f57a1dab865833e929b2
Eduardo Lopez Biagi <eduardo.biagi@gmail.com>
2012-12-17 23:51:53 -0600

    Third semantic pass to check data types and ranges
    Colors are unsigned integers.
    Better error reporting.

--- asmsemantic.py
+++ asmsemantic.py
@@ -1,15 +1,26 @@
 # Semantic analysis of an AST (parsed 68hc11 assembly code).
 
 from asmgrammar import Const, Inst, Var
+from bitstring import BitArray
 
 MAIN_ADDR = 0
 
-# memoria(0) := "11111111";
+errors = False
+error_no = 0
 
 def semantic_analysis(ast, const_table, data_table, inst_table):
+    global errors, error_no
+    errors = False
+    error_no = 0
+
     data = {'data_seg_size': 0, 'code_seg_size': 0}
     first_pass(ast, const_table, data_table, inst_table, data)
     second_pass(ast, const_table, data_table, inst_table, data)
+    third_pass(ast, const_table, data_table, inst_table, data)
+
+    if errors:
+        print "There are %d errors" % error_no
+        exit(1)
 
 def first_pass(ast, const_table, data_table, inst_table, data):
     """The first pass of semantic analysis records every constant and variable
@@ -18,25 +29,22 @@ def first_pass(ast, const_table, data_table, inst_table, data):
     for elem in ast:
         if isinstance(elem, Const):
             if elem.id in const_table:
-                print "WARNING: overriding already defined constant %s" % elem.id
+                warn("Overriding already defined constant %s", elem.id)
             const_table[elem.id] = elem.value
         elif isinstance(elem, Var):
             if elem.id in data_table:
-                print "ERROR: Duplicate name definition: %s" % elem.id
-                exit(1)
+                error("Duplicate name definition: %s", elem.id)
             data_table[elem.id] = -1
             data['data_seg_size'] += elem.size
         elif isinstance(elem, Inst):
             if elem.label != '':
                 if elem.label in inst_table:
-                    print "ERROR: Duplicate label definition: %s" % elem.label
-                    exit(1)
+                    error("Duplicate label definition: %s", elem.label)
                 inst_table[elem.label] = -1
             data['code_seg_size'] += elem.size
 
     if '.main' not in inst_table:
-        print "ERROR: Main entry point not defined"
-        exit(1)
+        error("Main entry point not defined")
 
 def second_pass(ast, const_table, data_table, inst_table, data):
     """The second pass assigns an address to variables (data segment) and
@@ -58,21 +66,54 @@ def second_pass(ast, const_table, data_table, inst_table, data):
             if len(elem.inst) == 3:
                 name, size, label = elem.inst
                 if label not in inst_table:
-                    print "ERROR: Undefined label %s" % label
-                    exit(1)
+                    error("Undefined label %s", label)
             elif len(elem.inst) == 4:
                 name, size, inst_type, value = elem.inst
                 if inst_type == 'const':
                     if value not in const_table:
-                        print "ERROR: Undefined constant %s" % value
-                        exit(1)
-                    elem.inst = (name, size, 'imm', const_table[value])
+                        error("Undefined constant %s", value)
+                    else:
+                        elem.inst = (name, size, 'imm', const_table[value])
                 elif inst_type == 'var':
                     if value not in data_table:
-                        print "ERROR: Undefined variable %s" % value
-                        exit(1)
-                    elem.inst = (name, size, 'dir', data_table[value])
+                        error("Undefined variable %s", value)
+                    else:
+                        elem.inst = (name, size, 'ext', data_table[value])
 
-    if inst_table['.main'] != 0:
-        print "ERROR: Main label should be the first instruction"
-        exit(1)
+    if inst_table['.main'] != MAIN_ADDR:
+        error("Main label should be the first instruction")
+
+def third_pass(ast, const_table, data_table, inst_table, data):
+    """The third pass handles unsigned values (color registers) and checks that
+    immediate values are of the correct size (one or two bytes).
+    """
+    for elem in ast:
+        if isinstance(elem, Inst):
+            if len(elem.inst) == 4:
+                name, size, inst_type, value = elem.inst
+                if name in {'LDB', 'LDG', 'LDR'}:
+                    value = BitArray(int=value, length=8).uint
+                    elem.inst = (name, size, inst_type, value)
+                if inst_type == 'imm':
+                    if name in {'LDB', 'LDG', 'LDR'}:
+                        if value < 0 or value > 255:
+                            error("Value out of range %s (instruction %s)", value, name)
+                    elif size == 2:
+                        if value < -128 or value > 127:
+                            error("Value out of range %s (instruction %s)", value, name)
+                    elif size == 3:
+                        if value < -32768 or value > 32767:
+                            error("Value out of range %s (instruction %s)", value, name)
+
+
+# TODO: line numbers
+def warn(msg, *args):
+    "Print a warning message."
+    print "WARNING: " + msg % args
+
+def error(msg, *args):
+    "Pring an error message, increment the global error count."
+    global errors, error_no
+    errors = True
+    error_no += 1
+    print "ERROR: " + msg % args
