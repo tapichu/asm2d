2167c5bebc6e3375f094e98ccdccd1fbeecf9add
Eduardo Lopez Biagi <eduardo.biagi@gmail.com>
2012-12-21 00:21:55 -0600

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def fread(fname):
     return open(join(dirname(__file__), fname)).read()
 
 setup(name = 'asm2d',
-      version = '0.1.3',
+      version = '0.1.4',
       description = 'An assembler for the 68112D microprocessor',
       long_description = fread('README.rst'),
       url = 'https://github.com/tapichu/asm2d',
